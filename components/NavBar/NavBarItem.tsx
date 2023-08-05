bc153c608f920be84bb5f4de4c7e6d5c92e9274b
Erick Llerenas <dev.llerenas@gmail.com>
2023-08-04 23:24:26 -0600

    product component and routes

---
+++ components/NavBar/NavBarItem.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+import { NavBarItemProps } from "./types";
+
+export default function NavBarItem({ title, icon, href }: NavBarItemProps) {
+  const path = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={classNames(
+        "flex flex-row items-center w-52 p-2 rounded-lg text-gray-700",
+        {
+          "text-orange-300": path === href,
+        }
+      )}
+    >
+      <i className={classNames(icon, "text-xl")}></i>
+      <span className="ml-4">{title}</span>
+    </Link>
+  );
+}
---
+++ components/NavBar/types.ts
@@ -0,0 +1,5 @@
+export type NavBarItemProps = {
+  title: string;
+  icon: string;
+  href: string;
+};
