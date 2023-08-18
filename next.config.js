d3c79847b0932229ac644fc3f3a0768e3195e86a
Erick Llerenas <dev.llerenas@gmail.com>
2023-08-17 23:25:40 -0600

    merged main

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["lh3.googleusercontent.com", "images.pexels.com"],
-  },
+	images: {
+		domains: ["lh3.googleusercontent.com", "photos.google.com"],
+	},
 };
 
 module.exports = nextConfig;
